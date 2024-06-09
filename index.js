99404108dd7ce1db5d1bd23412e6f89970f35826
miraybatki <123639768+miraybatki@users.noreply.github.com>
2024-06-09 22:10:59 +0000

    half footer and some updates

--- index.js
+++ index.js
@@ -2,3 +2,18 @@ function toggleMenu() {
     const menu = document.getElementById('menu');
     menu.classList.toggle('active');
 }
+
+function toggleMenu() {
+    const menu = document.getElementById('menu');
+    menu.classList.toggle('active');
+  }
+  
+  // Fotoğrafı basılı tutma olaylarını engelleme
+  const images = document.querySelectorAll('.no-drag');
+  
+  images.forEach(img => {
+    img.addEventListener('contextmenu', event => event.preventDefault());
+    img.addEventListener('mousedown', event => event.preventDefault());
+    img.addEventListener('touchstart', event => event.preventDefault());
+  });
+  
