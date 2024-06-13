8f2dd84cbc5c5f7cd94acad6a38603fe699b9b59
miraybatki <123639768+miraybatki@users.noreply.github.com>
2024-06-13 22:20:22 +0000

    footer ok :)

--- index.js
+++ index.js
@@ -1,19 +1,12 @@
 function toggleMenu() {
-    const menu = document.getElementById('menu');
-    menu.classList.toggle('active');
+  const menu = document.getElementById("menu");
+  menu.classList.toggle("active");
 }
 
-function toggleMenu() {
-    const menu = document.getElementById('menu');
-    menu.classList.toggle('active');
-  }
-  
-  // Fotoğrafı basılı tutma olaylarını engelleme
-  const images = document.querySelectorAll('.no-drag');
-  
-  images.forEach(img => {
-    img.addEventListener('contextmenu', event => event.preventDefault());
-    img.addEventListener('mousedown', event => event.preventDefault());
-    img.addEventListener('touchstart', event => event.preventDefault());
-  });
-  
+const images = document.querySelectorAll(".no-drag");
+
+images.forEach((img) => {
+  img.addEventListener("contextmenu", (event) => event.preventDefault());
+  img.addEventListener("mousedown", (event) => event.preventDefault());
+  img.addEventListener("touchstart", (event) => event.preventDefault());
+});
