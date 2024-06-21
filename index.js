093d0230cb5af6006b17caf90f6d7e844175703b
miraybatki <123639768+miraybatki@users.noreply.github.com>
2024-06-21 13:50:25 +0000

    touch action 3rd try

--- index.js
+++ index.js
@@ -9,3 +9,9 @@ images.forEach((img) => {
   img.addEventListener("contextmenu", (event) => event.preventDefault());
   img.addEventListener("mousedown", (event) => event.preventDefault());
 });
+
+document.addEventListener('touchstart', function(event) {
+  if (event.touches.length > 1) {
+      event.preventDefault();
+  }
+}, { passive: false });
