9b5e2412b2cc76f6ab7c96e958cddf5606a31b0d
miraybatki <123639768+miraybatki@users.noreply.github.com>
2024-06-21 23:22:05 +0000

    Hiç anlayamazsın baby

--- index.js
+++ index.js
@@ -14,4 +14,19 @@ document.addEventListener('touchstart', function(event) {
   if (event.touches.length > 1) {
       event.preventDefault();
   }
-}, { passive: false });
+}, { passive: false });   
+
+var coll = document.getElementsByClassName("collapsible");
+var i;
+
+for (i = 0; i < coll.length; i++) {
+  coll[i].addEventListener("click", function() {
+    this.classList.toggle("active");
+    var content = this.nextElementSibling;
+    if (content.classList.contains("show")) {
+      content.classList.remove("show");
+    } else {
+      content.classList.add("show");
+    }
+  });
+}
