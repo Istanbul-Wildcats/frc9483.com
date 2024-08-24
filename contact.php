10134517bd95fba12e965d81fe8fde63b4afe2e3
Miray Batkı <123639768+miraybatki@users.noreply.github.com>
2024-08-24 08:27:50 +0000

    slashh

--- contact.php
+++ contact.php
@@ -27,7 +27,7 @@ if ($_SERVER["REQUEST_METHOD"] == "POST") {
 <head>
     <meta charset="UTF-8" />
     <meta name="viewport" content="width=device-width, initial-scale=1.0, maximum-scale=1.0, user-scalable=0" />
-    <link rel="shortcut icon" href="img\logo.ico" type="image/x-icon" />
+    <link rel="shortcut icon" href="img/logo.ico" type="image/x-icon" />
     <title data-en="Team 9438 | Contact Form" data-tr="Team 9438 | İletişim Formu"></title>
     <style>
         @font-face {
